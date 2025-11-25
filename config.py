992eac97b2a7c2afad1ec5ddd769c300a5f94edf
benoit <benoit@getinside.fr>
2025-11-25 14:25:15 +0000

    Changes before Firebase Studio auto-run

---
+++ config.py
@@ -0,0 +1,10 @@
+
+GMAIL_USER = "your_email@gmail.com"
+GMAIL_PASSWORD = "your_password"
+TARGET_LABEL = "Github/archive-newsletters"
+OUTPUT_FOLDER = "docs"
+BATCH_SIZE = 9999
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
